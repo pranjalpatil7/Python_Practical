15e4318a33d3b662136192bb602190cd23890067
pranjalpatil7 <pranjal.236297208@vcet.edu.in>
2025-04-01 20:39:03 +0530

    Delete SE_45_Assignmnet/library directory

--- SE_45_Assignmnet/library/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import BookListCreateView, BookDetailView
-
-urlpatterns = [
-    # Book Endpoints
-    path('books/', BookListCreateView.as_view(), name='book-list-create'),
-    path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
-]
